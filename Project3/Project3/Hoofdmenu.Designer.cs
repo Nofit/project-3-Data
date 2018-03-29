39622d1810da37921b1f103d79f05161a8ef2a67
Nofit <33626766+Nofit@users.noreply.github.com>
2018-03-29 16:38:35 +0200

    big changes again

--- Project3/Project3/Hoofdmenu.Designer.cs
+++ Project3/Project3/Hoofdmenu.Designer.cs
@@ -39,22 +39,24 @@
             // 
             // buttonSub3
             // 
+            this.buttonSub3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.buttonSub3.Location = new System.Drawing.Point(532, 242);
             this.buttonSub3.Name = "buttonSub3";
             this.buttonSub3.Size = new System.Drawing.Size(216, 142);
             this.buttonSub3.TabIndex = 6;
-            this.buttonSub3.Text = "button3";
+            this.buttonSub3.Text = "Verhouding man/vrouw onder verdachten";
             this.buttonSub3.UseVisualStyleBackColor = true;
             this.buttonSub3.Click += new System.EventHandler(this.buttonSub3_Click);
             // 
             // buttonSub2
             // 
             this.buttonSub2.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.buttonSub2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.buttonSub2.Location = new System.Drawing.Point(292, 242);
             this.buttonSub2.Name = "buttonSub2";
             this.buttonSub2.Size = new System.Drawing.Size(216, 142);
             this.buttonSub2.TabIndex = 5;
-            this.buttonSub2.Text = "button2";
+            this.buttonSub2.Text = "Onveiligheidsgevoel";
             this.buttonSub2.UseVisualStyleBackColor = true;
             this.buttonSub2.Click += new System.EventHandler(this.buttonSub2_Click);
             // 
@@ -66,7 +68,7 @@
             this.buttonSub1.Name = "buttonSub1";
             this.buttonSub1.Size = new System.Drawing.Size(216, 142);
             this.buttonSub1.TabIndex = 4;
-            this.buttonSub1.Text = "Misdaad per provincie";
+            this.buttonSub1.Text = "Veiligheid over de jaren heen";
             this.buttonSub1.UseVisualStyleBackColor = true;
             this.buttonSub1.Click += new System.EventHandler(this.buttonSub1_Click);
             // 
@@ -87,10 +89,11 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(377, 462);
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(376, 468);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(42, 25);
+            this.label1.Size = new System.Drawing.Size(48, 28);
             this.label1.TabIndex = 8;
             this.label1.Text = "Exit";
             // 
@@ -99,11 +102,11 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Arial", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.WhiteSmoke;
-            this.label2.Location = new System.Drawing.Point(82, 107);
+            this.label2.Location = new System.Drawing.Point(68, 106);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(762, 38);
+            this.label2.Size = new System.Drawing.Size(787, 38);
             this.label2.TabIndex = 9;
-            this.label2.Text = "Crime in the Netherlands - Statistics Application";
+            this.label2.Text = "Criminaliteit in Nederland - Statistieken Applicatie";
             // 
             // Hoofdmenu
             // 
